710c6a06bc85aa6cd5ea5a55609ddfed37bd5881
SHENGYU LIU <45004947+LiuShengyu-Tech@users.noreply.github.com>
2025-10-03 16:21:09 +0200

    update

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,32 +1,63 @@
-import { Radar } from "lucide-react";
+import { useEffect, useState } from "react";
 
-interface SponsorProps {
-  icon: JSX.Element;
-  name: string;
-}
-
-const sponsors: SponsorProps[] = [
+const items = [
   {
-    icon: <Radar size={34} />,
-    name: "UNIVERSITY OF TWENTE",
+    logoDark: '/assets/logos/ut-white.png',
+    logoLight: '/assets/logos/ut-black.png',
+    link: 'https://www.utwente.nl/en/',
+    alt: 'Logo 1'
   },
   {
-    icon: <Radar size={34} />,
-    name: "NXTGEN HIGHTECH",
+    logoDark: '/assets/logos/NXTGEN.jpg',
+    logoLight: '/assets/logos/NXTGEN.jpg',
+    link: 'https://nxtgenhightech.nl/',
+    alt: 'Logo 1'
   },
   {
-    icon: <Radar size={34} />,
-    name: "UNEEDLE",
+    logoDark: '/assets/logos/uneedle.png',
+    logoLight: '/assets/logos/uneedle.png',
+    link: 'https://www.uneedle.com/',
+    alt: 'Logo 1'
   },
   {
-    icon: <Radar size={34} />,
-    name: "ODDBOX",
+    logoDark: '/assets/logos/oddbot.png',
+    logoLight: '/assets/logos/oddbot.png',
+    link: 'https://www.odd.bot/',
+    alt: 'Logo 2'
   },
-  
-
+  {
+    logoDark: '/assets/logos/cooll.png',
+    logoLight: '/assets/logos/cooll.png',
+    link: 'https://cooll.com/',
+    alt: 'Logo 2'
+  },
+  {
+    logoDark: '/assets/logos/distribute-white.png',
+    logoLight: '/assets/logos/distribute-black.png',
+    link: 'https://www.distribute.company/',
+    alt: 'Logo 2'
+  },
 ];
 
+
+
 export const Sponsors = () => {
+  const [isDark, setIsDark] = useState(document.documentElement.classList.contains('dark'));
+
+ useEffect(() => {
+    const observer = new MutationObserver(() => {
+      setIsDark(document.documentElement.classList.contains('dark'));
+    });
+
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
+
+    return () => observer.disconnect();
+  }, []);
+
+
+
+
+
   return (
     <section
       id="sponsors"
@@ -36,17 +67,21 @@ export const Sponsors = () => {
         Investors and Collaborating Companies
       </h2>
 
-      <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8">
-        {sponsors.map(({ icon, name }: SponsorProps) => (
-          <div
-            key={name}
-            className="flex items-center gap-1 text-muted-foreground/60"
-          >
-            <span>{icon}</span>
-            <h3 className="text-xl  font-bold">{name}</h3>
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-wrap justify-center items-center gap-4 md:gap-10">
+    {items.map((item, idx) => (
+      <a href={item.link} target="_blank" rel="noopener noreferrer" key={idx}>
+        <img 
+        src={isDark ? item.logoDark : item.logoLight} 
+        alt={item.alt} 
+        style={{height: 60 }} />
+      </a>
+    ))}
+  </div>
+
+
+
+
+
     </section>
   );
 };
